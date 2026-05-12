d1aaed0091fef8231b2001a67cbe64cd113c5855
th1enq <thienchy3305@gmail.com>
2026-05-12 21:06:22 +0700

    fix migration ver18

--- migrations/00018_unique_seat_map_name.sql
+++ migrations/00018_unique_seat_map_name.sql
@@ -1,6 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE UNIQUE INDEX uq_seat_maps_name
+WITH duplicate_active_seat_maps AS (
+  SELECT
+    id,
+    name,
+    ROW_NUMBER() OVER (
+      PARTITION BY name
+      ORDER BY created_at, id
+    ) AS duplicate_number
+  FROM seat_maps
+  WHERE deleted_at IS NULL
+)
+UPDATE seat_maps sm
+SET
+  name = duplicate_active_seat_maps.name || ' (' || sm.id::text || ')',
+  updated_at = now()
+FROM duplicate_active_seat_maps
+WHERE sm.id = duplicate_active_seat_maps.id
+  AND duplicate_active_seat_maps.duplicate_number > 1;
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_seat_maps_name
   ON seat_maps (name)
   WHERE deleted_at IS NULL;
 -- +goose StatementEnd
